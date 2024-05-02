c7fe4944f068021ecb9038bc99fd6f7bcf0d92e7
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-05-02 21:01:13 +0100

    refactor: move window creation to separate file

--- include/logging.hpp
+++ include/logging.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
-#ifdef _WIN32
-#include <windows.h>
+#include <iostream>
 #include <stdio.h>
 
+#ifdef _WIN32
+#include <windows.h>
+
 #define SETCOLOR(color) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+
+#define LOG_DEBUG(...) { \
+	SYSTEMTIME st; \
+	GetLocalTime(&st); \
+	SETCOLOR(11); printf("[%02d:%02d:%02d.%03d] ", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds); SETCOLOR(7); \
+	printf(__VA_ARGS__); \
+}
 
 #define LOG_INFO(...) { \
 	SYSTEMTIME st; \
@@ -20,29 +29,28 @@
 	printf(__VA_ARGS__); \
 }
 
-#ifdef DEBUG
-#define LOG_DEBUG(...) { \
-	SYSTEMTIME st; \
-	GetLocalTime(&st); \
-	SETCOLOR(11); printf("[%02d:%02d:%02d.%03d] ", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds); SETCOLOR(7); \
-	printf(__VA_ARGS__); \
-}
-#else
 #define LOG_ERROR(...) { \
 	SYSTEMTIME st; \
 	GetLocalTime(&st); \
 	SETCOLOR(12); printf("[%02d:%02d:%02d.%03d] ", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds); SETCOLOR(7); \
 	printf(__VA_ARGS__); \
-	MessageBoxA(NULL, __VA_ARGS__, "Error", MB_ICONERROR); \
+	std::cin.get(); \
 }
-#endif
 
 #elif __linux__
 
-#include <stdio.h>
 #include <time.h>
 
 #define SETCOLOR(color) printf("\033[%dm", color)
+
+#define LOG_DEBUG(...) { \
+    time_t rawtime; \
+    struct tm *timeinfo; \
+    time(&rawtime); \
+    timeinfo = localtime(&rawtime); \
+	SETCOLOR(34); printf("[%02d:%02d:%02d] ", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec); SETCOLOR(0); \
+	printf(__VA_ARGS__); \
+}
 
 #define LOG_INFO(...) { \
     time_t rawtime; \
@@ -69,6 +77,7 @@
     timeinfo = localtime(&rawtime); \
     SETCOLOR(31); printf("[%02d:%02d:%02d] ", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec); SETCOLOR(0); \
     printf(__VA_ARGS__); \
+	std::cin.get(); \
 }
 #endif
 
--- src/main.cpp
+++ src/main.cpp
@@ -11,82 +11,32 @@
 #include <stdio.h>
 
 #include "logging.hpp"
+
 #include "globals.cpp"
+#include "window.cpp"
 
 int main(void) {
-	GLFWwindow* window;
-
-	// Initialize the library
-	if (!glfwInit())
-		return -1;
-
-	// Create a windowed mode window and its OpenGL context
-	window = glfwCreateWindow(1280, 720, "ImGuiPlayGround", NULL, NULL);
-
-	if (!window) {
-		LOG_ERROR("Failed to create window");
-		glfwTerminate();
-		return -1;
-	}
-
-	LOG_INFO("Window created\n");
-
-	// Make the window's context current
-	glfwMakeContextCurrent(window);
-
-	LOG_INFO("Made context current\n");
-
-	// Initialize ImGui
-	IMGUI_CHECKVERSION();
-
-	LOG_INFO("ImGui version checked\n");
-
-	LOG_INFO("OpenGL version: %s\n", glGetString(GL_VERSION));
-
-#ifdef __linux__
-	LOG_INFO("GLSL version: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
-#endif
-
-	LOG_INFO("Renderer: %s\n", glGetString(GL_RENDERER));
-	LOG_INFO("GLFW version: %s\n", glfwGetVersionString());
-	LOG_INFO("ImGui version: %s\n", IMGUI_VERSION);
-
-	ImGui::CreateContext();
-	LOG_INFO("ImGui context created\n");
-
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-
-	// Setup Dear ImGui style
-	ImGui::StyleColorsDark();
-
-	// Setup Platform/Renderer bindings
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 130");
-
-	LOG_INFO("ImGui initialized\n");
-
-	ImGui::SetNextWindowSize(ImVec2(128, 128));
+	GLFWwindow* window = createWindow("ImGui playground", 1280, 720);
 
 	bool showDemo = true;
-	bool init = true;
 
 	// Loop until the user closes the window
 	while (!glfwWindowShouldClose(window) && !globals::shouldExit) {
 		// Update code here
+		// 
+		// Update code here
 
-		glfwPollEvents();
 
 		// Start the ImGui frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		if (init) {
-			ImGui::SetNextWindowSize(ImVec2(256, 128));
-			init = false;
-		}
-
 		// Render code here
+		// 
+		// Render code here
+
+		ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiCond_FirstUseEver);
 		ImGui::Begin("ImGui playground");
 
 		if (ImGui::Button("Exit"))
@@ -105,6 +55,8 @@ int main(void) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		glfwSwapBuffers(window);
+
+		glfwPollEvents();
 	}
 
 	// Cleanup
